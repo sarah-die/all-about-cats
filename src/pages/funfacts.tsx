d668f943dd192b5648757240ff9ae4a1f2e0d698
sarah-die <saridi@alice.de>
2023-05-22 16:11:47 +0200

    New Page funfacts

---
+++ src/pages/funfacts.tsx
@@ -0,0 +1,13 @@
+import Layout from "@/components/Layout";
+import Head from "next/head";
+
+export default function Funfacts() {
+    return (
+        <Layout>
+            <Head>
+                <title>Funfacts</title>
+            </Head>
+            <div>Add something like "Effect Flip" here</div>
+        </Layout>
+    )
+}
