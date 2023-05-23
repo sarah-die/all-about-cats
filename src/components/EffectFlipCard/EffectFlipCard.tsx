3655048b0fbd42c76e7eb24f61313de09f5d8f11
sarah-die <saridi@alice.de>
2023-05-23 12:21:26 +0200

    Effect flip cards for the funfacts are working

---
+++ src/components/EffectFlipCard/EffectFlipCard.tsx
@@ -0,0 +1,33 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { EffectFlip } from "swiper";
+import image from "../../../public/20230103_155348.jpg";
+import Image from "next/image";
+
+export default function EffectFlipCard({ url, funfact }) {
+  return (
+    <div className="effect-flip-card">
+        <Swiper
+          effect={"flip"}
+          grabCursor={true}
+          loop={true}
+          modules={[EffectFlip]}
+        >
+          <SwiperSlide>
+            <Image
+              src={image}
+              alt={"this is an image"}
+              height={400}
+              width={400}
+            ></Image>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="card-style has-text-centered">
+              <div className="is-size-5-mobile is-size-4 backside-text">
+                This is some funfact Lorem ipsum dolor sit amet, consectetur.
+              </div>
+            </div>
+          </SwiperSlide>
+        </Swiper>
+    </div>
+  );
+}
--- src/pages/funfacts.tsx
+++ src/pages/funfacts.tsx
@@ -1,13 +1,17 @@
 import Layout from "@/components/Layout";
 import Head from "next/head";
+import EffectFlipCard from "@/components/EffectFlipCard/EffectFlipCard";
 
 export default function Funfacts() {
-    return (
-        <Layout>
-            <Head>
-                <title>Funfacts</title>
-            </Head>
-            <div>Add something like "Effect Flip" here</div>
-        </Layout>
-    )
+  return (
+    <Layout>
+      <Head>
+        <title>Funfacts</title>
+      </Head>
+      <div>Turn the cards/ pictures around for some fun facts</div>
+      <EffectFlipCard />
+      <EffectFlipCard />
+      <EffectFlipCard />
+    </Layout>
+  );
 }
