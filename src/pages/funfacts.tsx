cb454ef31e8c2b770194e354ce487f1bb7f20324
sarah-die <saridi@alice.de>
2023-05-23 14:18:09 +0200

    Fixed issue (Grid columns causing horizontal scrolling) as mentioned here: https://github.com/jgthms/bulma/issues/2242

--- src/pages/funfacts.tsx
+++ src/pages/funfacts.tsx
@@ -1,6 +1,12 @@
 import Layout from "@/components/Layout";
 import Head from "next/head";
 import EffectFlipCard from "@/components/EffectFlipCard/EffectFlipCard";
+import image from "../../public/20230103_155348.jpg";
+
+const funfactsData = new Array(9).fill({
+  url: image,
+  funfact: "This is some funfact Lorem ipsum dolor sit amet, consectetur.",
+});
 
 export default function Funfacts() {
   return (
@@ -8,10 +14,20 @@ export default function Funfacts() {
       <Head>
         <title>Funfacts</title>
       </Head>
-      <div>Turn the cards/ pictures around for some fun facts</div>
-      <EffectFlipCard />
-      <EffectFlipCard />
-      <EffectFlipCard />
+      <section className="section">
+        Turn the cards/ pictures around for some fun facts
+      </section>
+      <div className="container">
+        <div className="columns is-multiline is-gapless">
+          {funfactsData.map((el, index) => {
+            return (
+              <div className="column" key={index}>
+                <EffectFlipCard url={el.url} funfact={el.funfact} />
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </Layout>
   );
 }
