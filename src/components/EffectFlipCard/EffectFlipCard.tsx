16c92f5622b9aa016f1ac24c01bd093062c205bb
sarah-die <saridi@alice.de>
2023-05-24 10:01:31 +0200

    Added type declarations on parameters

--- src/components/EffectFlipCard/EffectFlipCard.tsx
+++ src/components/EffectFlipCard/EffectFlipCard.tsx
@@ -3,31 +3,34 @@ import { EffectFlip } from "swiper";
 import image from "../../../public/20230103_155348.jpg";
 import Image from "next/image";
 
-export default function EffectFlipCard({ url, funfact }) {
+export default function EffectFlipCard(props: {
+  url: string;
+  funfact: string;
+}) {
   return (
     <div className="effect-flip-card">
-        <Swiper
-          effect={"flip"}
-          grabCursor={true}
-          loop={true}
-          modules={[EffectFlip]}
-        >
-          <SwiperSlide>
-            <Image
-              src={image}
-              alt={"this is an image"}
-              height={400}
-              width={400}
-            ></Image>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="card-style has-text-centered">
-              <div className="is-size-5-mobile is-size-4 backside-text">
-                This is some funfact Lorem ipsum dolor sit amet, consectetur.
-              </div>
+      <Swiper
+        effect={"flip"}
+        grabCursor={true}
+        loop={true}
+        modules={[EffectFlip]}
+      >
+        <SwiperSlide>
+          <Image
+            src={image}
+            alt={"this is an image"}
+            height={400}
+            width={400}
+          ></Image>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="card-style has-text-centered">
+            <div className="is-size-5-mobile is-size-4 backside-text">
+              This is some funfact Lorem ipsum dolor sit amet, consectetur.
             </div>
-          </SwiperSlide>
-        </Swiper>
+          </div>
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 }
