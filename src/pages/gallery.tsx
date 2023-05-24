45ab43439030bf1e0b56048301f6407ed37bd1e4
sarah-die <saridi@alice.de>
2023-05-24 15:55:35 +0200

    Added content to gallery page

--- src/pages/gallery.tsx
+++ src/pages/gallery.tsx
@@ -14,9 +14,25 @@ export default function Gallery() {
           Hier ein paar Foto aus den letzten Monaten. Viel Spaß beim Anschauen!
         </div>
       </div>
-      <SquareSlider urlPrefix={"month1to6"} numberOfImages={26}/>
-      <SquareSlider urlPrefix={"month6to12"} numberOfImages={24}/>
-      <SquareSlider urlPrefix={"month12to18"} numberOfImages={7}/>
+      <div className="text-container">
+        <div className="subtitle">
+          Unsere ersten sechs Monate. Von März 2022 bis September 2022.
+        </div>
+      </div>
+      <SquareSlider urlPrefix={"month1to6"} numberOfImages={26} />
+      <div className="text-container">
+        <div className="subtitle">
+          Unser zweites halbes Jahr. Von September 2022 bis März 2023.
+        </div>
+      </div>
+      <SquareSlider urlPrefix={"month6to12"} numberOfImages={24} />
+      <div className="text-container">
+        <div className="subtitle">
+          Endlich erwachsen - mit 1 Jahr geht's rund! März 2023 bis September
+          2023.
+        </div>
+      </div>
+      <SquareSlider urlPrefix={"month12to18"} numberOfImages={7} />
     </Layout>
   );
 }
