4951cf63d19bf5629d1a514fc1bcebb4f0421adb
sarah-die <saridi@alice.de>
2023-06-01 09:44:21 +0200

    disable eslint for github pages

--- src/pages/gallery.tsx
+++ src/pages/gallery.tsx
@@ -1,3 +1,4 @@
+// eslint-disable react/no-unescaped-entities
 import Layout from "@/components/Layout";
 import Head from "next/head";
 import { SquareSlider } from "@/components/SquareSlider/SquareSlider";
